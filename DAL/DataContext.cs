89d530df40eb41d4a6f06183ee52bfae71152b57
MrTheoslav <wo303232@student.polsl.pl>
2024-03-26 11:35:33 +0100

    Dodaj pliki projektów.

---
+++ DAL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace DAL
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        
+        }
+        
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }   
+}
---
+++ Model/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count {  get; set; }
+    }
+}
